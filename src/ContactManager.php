fa838e4d10aa2a7c55c4c427f8c3b6bddef981f4
Svetoslav Mishkov <svetoslav.mishkov@gmail.com>
2014-10-30 22:32:27 +0200

    Added comments.

--- src/ContactManager.php
+++ src/ContactManager.php
@@ -17,6 +17,12 @@ class ContactManager extends EntityManager
     ];
     protected $lookupField = 'email';
 
+    /**
+     * Fixes the attributes property. It should not be null.
+     *
+     * @param \Gan\Entity $entity
+     * @return object
+     */
     protected function normalizeEntity(Entity $entity) {
         $data = parent::normalizeEntity($entity);
         if (!is_object($data->attributes)) {
